6199f384b32cae7530a0503411203e907b46d274
ali_eren <alierenunal957@gmail.com>
2023-07-09 22:53:28 +0300

    update navigation.

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,49 +1,76 @@
-// Get the elements which has the class name value of 'heading'.
-const headings = document.getElementsByClassName("heading");
+// Get all the heading elements.
+const HEADING_ELEMENTS = document.getElementsByClassName("heading");
+
+// Declare title depth counter.
+let titleDepthCounter = [];
+
+// Get the heading IDs from the elements.
 const HEADING_ID_LIST = [];
+const NAVIGATION_LIST_ELEMENT = document.getElementById("navigation-list");
 
-for(let index = 0; index < headings.length; index++) {
-    const heading_id = headings[index].getAttribute("id");
+for(let index = 0; index < HEADING_ELEMENTS.length; index++) {
+    const HEADING_ID = HEADING_ELEMENTS[index].getAttribute("id");
 
-    if(heading_id !== null) {
-        HEADING_ID_LIST.push(heading_id);
-    }
-}
+    if(HEADING_ID !== null) {
+        HEADING_ID_LIST.push(HEADING_ID);
 
-const NAVIGATION_LINKS = [];
+        // Convert the heading IDs into capitalized format.
+        const PARSED_ID_TEXT = HEADING_ID.split("-");
+        let navigation_link = "";
 
-// Convert the link texts into capitalized format.
-for(let index = 0; index < HEADING_ID_LIST.length; index++) {
-    const parsedIdText = HEADING_ID_LIST[index].split("-");
-    let navigation_link = "";
-
-    for(let index_2 = 0; index_2 < parsedIdText.length; index_2++) {
-        const firstCharacter = parsedIdText[index_2].charAt(0);
-        const capitalizedFirstCharacter = firstCharacter.toUpperCase();
-        const capitalizedText = capitalizedFirstCharacter + parsedIdText[index_2].slice(1);
-
-        navigation_link += ` ${capitalizedText}`
-    }
+        for(let index_2 = 0; index_2 < PARSED_ID_TEXT.length; index_2++) {
+            const FIRST_CHARACTER = PARSED_ID_TEXT[index_2].charAt(0).toUpperCase();
+            const CAPITALIZED_TEXT = FIRST_CHARACTER + PARSED_ID_TEXT[index_2].slice(1);
     
-    NAVIGATION_LINKS.push(navigation_link);
+            navigation_link += ` ${CAPITALIZED_TEXT}`
+        }
+
+        // Create navigation list.
+        // In this section, title depth and its styling also take into account.
+        const TITLE_DEPTH = HEADING_ELEMENTS[index].classList[2];
+
+        if(TITLE_DEPTH !== undefined) {
+            const TITLE_DEPTH_LENGTH = TITLE_DEPTH.length;
+
+            // If the n-th array is not available, create one and assing '0' to it.
+            if(titleDepthCounter[TITLE_DEPTH_LENGTH - 1] === undefined) {
+                titleDepthCounter[TITLE_DEPTH_LENGTH - 1] = 0;
+            }
+
+            // If depth length is less than the array size, reset some part of the counter.
+            // For example if the next number is '6' and the current counter is '5.8', pop the '8'.
+            // Because there might be a subtitle in '6'. If it is the case, the counter should start from the '0', not the  '8'.
+
+            if(TITLE_DEPTH_LENGTH < titleDepthCounter.length){
+                for(let index_2 = 0; index_2 < (titleDepthCounter.length - TITLE_DEPTH_LENGTH); index_2++) {
+                    titleDepthCounter.pop();
+                }
+            }
+
+            titleDepthCounter[TITLE_DEPTH_LENGTH - 1] += 1;
+
+            // Cereate the number to be assigned as an ordered list.
+            let titleNumber = "";
+
+            for(let index_2 = 0; index_2 < TITLE_DEPTH_LENGTH; index_2++) {
+                titleNumber += `${titleDepthCounter[index_2]}.`;
+            }
+
+            const P_ELEMENT = document.createElement("p");
+            P_ELEMENT.textContent = `${titleNumber} `;
+            P_ELEMENT.style.marginLeft = `${8 * (TITLE_DEPTH_LENGTH - 1)}px`;
+
+            const A_ELEMENT = document.createElement("a");
+            A_ELEMENT.textContent = navigation_link;
+            A_ELEMENT.setAttribute("href", `#${HEADING_ID}`);
+
+            P_ELEMENT.appendChild(A_ELEMENT);
+            NAVIGATION_LIST_ELEMENT.appendChild(P_ELEMENT);
+        }
+    }
 }
 
-// Put navigation links inside unordered list.
-const navigationListElement = document.getElementById("navigation-list");
-
-for(let index = 0; index < NAVIGATION_LINKS.length; index++) {
-    const newParagraphElement = document.createElement("p");
-    newParagraphElement.textContent = `${index + 1}. `;
-
-    const newLinkElement = document.createElement("a");
-    newLinkElement.textContent = NAVIGATION_LINKS[index];
-
-    newLinkElement.setAttribute("href", `#${HEADING_ID_LIST[index]}`);
-    newParagraphElement.appendChild(newLinkElement);
-    navigationListElement.appendChild(newParagraphElement);
-}
-
-// Make navigation visible.
+// Display navigation.
 let window_width = window.screen.width;
 displayNavigation(window_width);
 
@@ -52,17 +79,18 @@ window.onresize = function() {
     displayNavigation(window_width);
 };
 
+// FUNCTIONS
 function displayNavigation(windowWidth) {
     if(HEADING_ID_LIST.length !== 0) {
-        const navigationElement = document.getElementById("navigation");
-        const scrollToTop = document.getElementById("scroll-to-top");
+        const NAVIGATION_ELEMENT = document.getElementById("navigation");
+        const SCROLL_TO_TOP = document.getElementById("scroll-to-top");
 
         if(windowWidth > 1199) {
-            navigationElement.style.display = "flex";
-            scrollToTop.style.display = "none";
+            NAVIGATION_ELEMENT.style.display = "flex";
+            SCROLL_TO_TOP.style.display = "none";
         } else {
-            navigationElement.style.display = "none";
-            scrollToTop.style.display = "flex";
+            NAVIGATION_ELEMENT.style.display = "none";
+            SCROLL_TO_TOP.style.display = "flex";
         }
     }
 }
